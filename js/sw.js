6d710724cda1cbb0e042265992cb74fbe5e8e5a9
AndriiLytvyn <andriilytvin93@gmail.com>
2024-06-15 10:18:29 +0200

    add scc style

---
+++ js/sw.js
@@ -0,0 +1,33 @@
+let  cachedName="podyplomowa"
+
+let  fileToCache=[ "./", "./index.html" , "./style/style.css" , "./js/main.js"]
+
+self.addEventListener('install', function(event) {
+     // Perform install steps
+     console.log('installing sw');
+     event.waitUntil(
+     caches.open(CACHE_NAME)
+     .then(function(cache) {
+     console.log('Opened cache');
+     var x = cache.addAll(urlsToCache);
+     console.log('cache added');
+     return x;
+     })
+     
+     );
+     });
+     
+     self.addEventListener('fetch', function(event) {
+     event.respondWith(
+     caches.match(event.request)
+     .then(function(response) {
+     
+     // Cache hit - return response
+     if (response) {
+     return response;
+     }
+     return fetch(event.request);
+}
+)
+);
+});
