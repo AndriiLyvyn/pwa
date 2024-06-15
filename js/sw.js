1ae040f112816039a14eaaae2b1507a111107acf
AndriiLytvyn <andriilytvin93@gmail.com>
2024-06-15 11:13:14 +0200

    add ms,js

--- js/sw.js
+++ js/sw.js
@@ -1,15 +1,15 @@
-let  cachedName="podyplomowa"
 
-let  fileToCache=[ "./", "./index.html" , "./style/style.css" , "./js/main.js"]
+let cacheName = "podyplomowe";
+let filesToCache = ["./", "./index.html", "./style/style.css",
+"./js/main.js"];
 
 self.addEventListener('install', function(event) {
-     // Perform install steps
      console.log('installing sw');
      event.waitUntil(
      caches.open(CACHE_NAME)
      .then(function(cache) {
      console.log('Opened cache');
-     var x = cache.addAll(urlsToCache);
+     let x = cache.addAll(urlsToCache);
      console.log('cache added');
      return x;
      })
